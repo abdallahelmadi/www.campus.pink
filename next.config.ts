350cf023113d48a79ae83ec1418b39ebc57d5154
abdallahelmadi <abdallahelmadi42@gmail.com>
2026-03-18 22:25:49 +0000

    wip

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  cacheComponents: true,
   images: {
     qualities: [70, 80, 90, 100],
     remotePatterns: [
