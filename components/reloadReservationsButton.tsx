b287457a2b2e16ff1ad5d5ca6853e8a6a0bb57c8
abdallahelmadi <abdallahelmadi42@gmail.com>
2026-03-19 01:21:15 +0000

    wip

--- components/reloadReservationsButton.tsx
+++ components/reloadReservationsButton.tsx
@@ -27,8 +27,9 @@ export default function ReloadReservationsButton(): React.JSX.Element {
 
   return (
     <div
-      className="h-10 w-10 bg-[hsla(0,0%,100%,1)] shadow-[0_0_0_1px_#00000014]
-      rounded-md flex items-center justify-center select-none overflow-hidden cursor-pointer"
+      className="h-10 w-10 bg-white shadow-[0_0_0_1px_#00000014]
+      rounded-md flex items-center justify-center select-none overflow-hidden cursor-pointer
+      transition duration-200 ease-in-out hover:bg-gray-100"
       onClick={handleClick}
     >
       <style>{`
