60f550ad4a50f95e405b7ec38c0f84ecd1bb9a6f
abdallahelmadi <abdallahelmadi42@gmail.com>
2026-03-19 23:23:34 +0000

    wip

--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -17,18 +17,9 @@ export default async function Reservations(): Promise<React.JSX.Element> {
         <Header user={user} />
 
         <div className="flex flex-col">
-          <div className="flex items-center justify-between w-full">
-            <span className="text-black font-medium">
-              Your Reservations
-            </span>
-          </div>
-          <div className="mt-1">
-
-            <Suspense fallback={<ReservationSkeleton />}>
-              <ReservationsGrid token={token} />
-            </Suspense>
-
-          </div>
+          <Suspense fallback={<ReservationSkeleton />}>
+            <ReservationsGrid token={token} />
+          </Suspense>
         </div>
 
         <Footer />
