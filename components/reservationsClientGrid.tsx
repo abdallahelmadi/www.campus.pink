2218f154a6e89620cf4bd47166ca885c5e712ed4
abdallahelmadi <abdallahelmadi42@gmail.com>
2026-03-19 01:28:38 +0000

    wip

--- components/reservationsClientGrid.tsx
+++ components/reservationsClientGrid.tsx
@@ -98,7 +98,7 @@ export default function ReservationsClientGrid({
           <div
             suppressHydrationWarning
             className="grid grid-cols-1 sm:grid-cols-2 w-full max-w-340
-            lg:grid-cols-3 xl:grid-cols-4 gap-4"
+            lg:grid-cols-3 xl:grid-cols-4 gap-2"
           >
 
             {reservations.map(reservation => (
