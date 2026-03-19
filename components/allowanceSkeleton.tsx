c37820c7106dc4b584dc33092b04ae002ce8afa6
abdallahelmadi <abdallahelmadi42@gmail.com>
2026-03-19 03:57:22 +0000

    wip

--- components/allowanceSkeleton.tsx
+++ components/allowanceSkeleton.tsx
@@ -13,10 +13,18 @@ export default function AllowanceSkeleton(): React.JSX.Element {
 
       <div className="mt-2 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
         {Array.from({ length: 11 }).map((_, index) => (
-          <Skeleton
+          <div
             key={index}
-            className="w-full! h-[252.5px] rounded-lg!"
-          />
+            className="relative border border-gray-200 rounded-lg overflow-hidden"
+          >
+            <Skeleton className="w-full! h-36! overflow-hidden!"/>
+            <div className="p-2 flex flex-col gap-1">
+              <Skeleton className="rounded-md! w-27! h-4!"/>
+              <div className="flex items-center gap-3 flex-wrap">
+                <Skeleton className="rounded-md! w-12.5! h-4!"/>
+              </div>
+            </div>
+          </div>
         ))}
       </div>
 
