fb07dd41f5488a836130fa00a98efcd2639caad9
abdallahelmadi <abdallahelmadi42@gmail.com>
2026-03-19 07:43:47 +0000

    wip

--- app/services/[service_id]/[allowance_id]/page.tsx
+++ app/services/[service_id]/[allowance_id]/page.tsx
@@ -1,3 +1,9 @@
+import Header from "@/components/header"
+import { getUser, getAllowances, getServices } from "@/actions"
+import { notFound } from "next/navigation"
+import type { Allowance } from "@/interfaces"
+import Breadcrumb from "@/components/breadcrumb"
+
 export default async function ServiceById({
   params
 }: {
@@ -8,7 +14,35 @@ export default async function ServiceById({
   const serviceId = Number(service_id)
   const allowanceId = Number(allowance_id)
 
+  if (isNaN(serviceId)) notFound()
+  if (isNaN(allowanceId)) notFound()
+
+  const user = await getUser()
+  if (!user) notFound()
+
+  const services = await getServices(user.token)
+  const service = services.find(s => s.id === serviceId)
+  if (!service) notFound()
+
+  const allowances: Allowance[] = await getAllowances(user.token, serviceId)
+  const allowance = allowances.find(a => a.id === allowanceId)
+  if (!allowance) notFound()
+
   return (
-    <> {serviceId} - {allowanceId} </>
+    <main className="flex flex-col items-center gap-4 w-full p-2 mt-14">
+      <main className="max-w-340 w-full flex flex-col gap-1">
+    
+        <Header user={user} />
+
+        <Breadcrumb
+          elements={[
+            { href: "/services", label: "Services" },
+            { href: `/services/${serviceId}`, label: service.name || "service" },
+            { href: `/services/${serviceId}/${allowanceId}`, label: allowance.name || "allocation" }
+          ]}
+        />
+      
+      </main>
+    </main>
   )
 }
