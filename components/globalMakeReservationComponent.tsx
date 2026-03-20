d1023423005699e00473e3c318735d6dbd9e2024
abdallahelmadi <abdallahelmadi42@gmail.com>
2026-03-20 03:52:49 +0000

    wip

--- components/globalMakeReservationComponent.tsx
+++ components/globalMakeReservationComponent.tsx
@@ -79,8 +79,6 @@ export default async function GlobalMakeReservationComponent({
 
       <AllowanceFullCard allowance={allowance}/>
 
-      <div className="mt-2"/>
-
       <TitleSection title="SELECT DATE"/>
 
       <DatePickerStrip
