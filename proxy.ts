dc1f66be00779303c2b19db890840f9ea1156b79
abdallahelmadi <abdallahelmadi42@gmail.com>
2026-04-06 21:09:56 +0100

    wip

--- proxy.ts
+++ proxy.ts
@@ -2,13 +2,11 @@ import { NextRequest, NextResponse } from "next/server"
 import { getUser } from "@/actions"
 
 export async function proxy(request: NextRequest): Promise<NextResponse<unknown>> {
-
   const { pathname } = request.nextUrl
   const t = await getUser()
 
   if (pathname.startsWith("/login") && t)
     return NextResponse.redirect(new URL("/", request.url))
-
   if (!t && !pathname.startsWith("/login"))
     return NextResponse.redirect(new URL("/login", request.url))
 
